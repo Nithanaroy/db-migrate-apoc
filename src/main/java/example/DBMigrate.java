55a4a8407a573a4543d6371b6aaf81fcd92510e2
Nitin Pasumarthy <npasumarthy@apple.com>
2017-10-27 17:53:46 -0700

    Changed migration filename convention. Added procedure for creating a migration file

--- src/main/java/example/DBMigrate.java
+++ src/main/java/example/DBMigrate.java
@@ -1,9 +1,16 @@
 package example;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.stream.Stream;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -27,25 +34,30 @@ public class DBMigrate {
 
 	@Procedure(value = "example.upgrade", mode = Mode.WRITE)
 	@Description("Upgrades the DB to the latest version")
-	public void upgrade(@Name("folder") String folder) {
-		System.out.println("Hello World from upgrade");
+	public Stream<Message> upgrade(@Name("folder") String folder) {
+		System.out.println("Running migration, upgrade");
 		try {
 			long currentVersion = currentDBVersion();
+			long lastKnownVersion = currentVersion; // stores the last version
+													// ran
 			JSONParser parser = new JSONParser();
 			File[] files = filesToMigrate(folder, currentVersion);
 
 			for (File file : files) {
+				lastKnownVersion = getVersionFromFile(file);
 				System.out.println(String.format("Running migration file, '%s'", file.getName()));
-				long version = getVersionFromFile(file);
 				JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(file.getAbsoluteFile()));
 				String cypher = (String) jsonObject.get("upgrade");
 				System.out.println(String.format("Executing cypher, '%s'", cypher));
 				Transaction t = db.beginTx();
 				db.execute(cypher);
-				db.execute(String.format(setDBVersionCypher, version));
+				db.execute(String.format(setDBVersionCypher, lastKnownVersion));
 				t.success();
 				t.close();
 			}
+			return Stream.of(new Message[] { new Message(
+					String.format("Upgraded DB to latest version, '%s' from '%s' by running %s migration scripts",
+							lastKnownVersion, currentVersion, files.length)) });
 		} catch (Exception e) {
 			throw new RuntimeException(e); // this type is required by neo4j
 		}
@@ -65,9 +77,14 @@ public class DBMigrate {
 		File[] files = new File(folder).listFiles();
 		for (File file : files) {
 			if (file.isFile()) {
-				long fileVersion = getVersionFromFile(file);
-				if (fileVersion > version) {
-					resultList.add(file);
+				try {
+					long fileVersion = getVersionFromFile(file);
+					if (fileVersion > version) {
+						resultList.add(file);
+					}
+				} catch (NumberFormatException e) {
+					System.out.println(String.format(
+							"'%s' does not match migration file naming convention, skipping it.", file.getName()));
 				}
 			}
 		}
@@ -85,21 +102,46 @@ public class DBMigrate {
 	}
 
 	/**
-	 * Extracts the version number from file name
+	 * Extracts the version number from file name Example filename:
+	 * /migrations/23424242424-myMigration.json where version is 23424242424
 	 * 
 	 * @param file
 	 *            instance of file object to get the name from
 	 * @return version number
 	 */
 	private long getVersionFromFile(File file) {
-		long fileVersion = Long.parseLong(file.getName().split("-")[1].split("[.]")[0]);
-		return fileVersion;
+		return Long.parseLong(file.getName().split("-")[0]);
 	}
 
 	@Procedure("example.createMigrationFile")
-	@Description("Creates a new migration file")
-	public void createMigrationFile(@Name("folder") String folder) {
-		System.out.println("Hello from create migration file");
+	@Description("Creates a new migration file in the folder specified with the given migration name")
+	public Stream<Message> createMigrationFile(@Name("folder") String folder, @Name("name") String name) {
+		System.out.println("Running create migration file");
+		try {
+			String fileContent = "{\"upgrade\": \"\", \"downgrade\": \"\", \"description\": \"\" }";
+			String fileName = String.format("%s-%s.json", new Timestamp(System.currentTimeMillis()).getTime(), name);
+			Path filepath = Paths.get(folder, fileName).toAbsolutePath();
+			createFileWithContent(filepath.toString(), fileContent);
+			return Stream.of(new Message[] { new Message(String
+					.format("Created a template migration file for you to update at '%s'", filepath.toString())) });
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void createFileWithContent(String filepath, String content) throws IOException {
+		File file = new File(filepath);
+		file.createNewFile();
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(content);
+		bw.close();
+	}
+
+	@Procedure(value = "example.version", mode = Mode.READ)
+	@Description("Find out the current DB version")
+	public Stream<Message> version() {
+		return Stream.of(new Message[] { new Message(String.format("%s", currentDBVersion())) });
 	}
 
 	public long currentDBVersion() {
@@ -110,5 +152,13 @@ public class DBMigrate {
 			currentVersion = (long) n.getProperty("version");
 		}
 		return currentVersion;
+	}
+
+	public class Message {
+		public String message;
+
+		public Message(String message) {
+			this.message = message;
+		}
 	}
 }
